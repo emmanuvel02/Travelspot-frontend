b2c5f8dfeda8282acff121004653a47f245ba7d2
emmanuvel02 <saaneeshsaji200423@gmail.com>
2025-04-27 10:37:35 +0530

    fixbug

--- src/configure/api.js
+++ src/configure/api.js
@@ -1,2 +1,4 @@
-export const userApi="http://localhost:3001/"
-export const adminApi="http://localhost:3001/admin"
+// export const userApi="http://localhost:3001/"
+// export const adminApi="http://localhost:3001/admin"
+export const userApi="https://travelspot-backend.onrender.com/"
+export const adminApi="https://travelspot-backend.onrender.com/admin"
